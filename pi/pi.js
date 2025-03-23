b6c41b22e8501ef36af49144dd30a135d16efc14
Tjok Agung <tjokagungpdp@gmail.com>
2025-03-23 18:08:14 +0100

    best approx

--- pi/pi.js
+++ pi/pi.js
@@ -4,6 +4,10 @@ let inside = 0
 
 let increment // for incrementer
 
+let bestApprox = 0
+let pointsAtBest = 0
+let insideAtBest = 0
+
 function isPointInside() {
     let point = [2 * Math.random() - 1, 2 * Math.random() - 1]
 
@@ -18,15 +22,32 @@ function piApproximation() {
     return (inside / points) * 4
 }
 
+function gcd(a, b) {
+    while (b !== 0) {
+        [a, b] = [b, a % b]
+    }
+
+    return a
+}
+
 function updatePi() {
     points += 1
 
     isPointInside()
     const approx = piApproximation()
 
+    if (Math.abs(approx - Math.PI) < Math.abs(bestApprox - Math.PI)) {
+        bestApprox = approx
+        pointsAtBest = points
+        insideAtBest = inside
+    }
+
     document.getElementById("approx").innerHTML = `π ≈ ${approx}`
     document.getElementById("error").innerHTML = `Error = ${approx - Math.PI}`
     document.getElementById("points").innerHTML = `${points} point(s) (${inside} inside, ${outside} outside)`
+
+    document.getElementById("best").innerHTML = `Best approximation: ${bestApprox} (${insideAtBest * 4 / gcd(pointsAtBest, insideAtBest * 4)}/${pointsAtBest / gcd(pointsAtBest, insideAtBest * 4)})`
+    document.getElementById("besterror").innerHTML = `Error = ${bestApprox - Math.PI}`
 }
 
 function incrementer() {
@@ -42,9 +63,16 @@ function reset() {
     outside = 0
     inside = 0
 
+    bestApprox = 0
+    pointsAtBest = 0
+    insideAtBest = 0
+
     stopIncrementer()
 
     document.getElementById("approx").innerHTML = `π ≈ 0`
     document.getElementById("error").innerHTML = `Error = ${0 - Math.PI}`
     document.getElementById("points").innerHTML = `${points} point(s) (${inside} inside, ${outside} outside)`
+
+    document.getElementById("best").innerHTML = `Best approximation: ${bestApprox} (N/A)`
+    document.getElementById("besterror").innerHTML = `Error = ${bestApprox - Math.PI}`
 }
